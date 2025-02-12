560c60344773a1c54ab955a3067ecfe155d78df0
Kevin Lam <klam@lcs.on.ca>
2025-02-12 08:42:18 -0500

    Initial Commit

---
+++ PropertyWrappers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PropertyWrappers
+//
+//  Created by Kevin Lam on 12/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
